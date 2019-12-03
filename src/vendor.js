101eb450816dbce1db938cbbd3796b05e88f3a6c
IzabellaHe <izabella.hermanowicz@gmail.com>
2019-12-03 16:56:54 +0100

    Fetching data from json to console

--- src/vendor.js
+++ src/vendor.js
@@ -1,1 +1,11 @@
 import 'bootstrap';
+
+async function getPokemon(name) 
+{
+  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  let data = await response.json()
+  return data;
+}
+
+getPokemon('ditto')
+  .then(data => console.log(data)); 
